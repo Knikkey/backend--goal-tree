acffb7787b2891736e4d0b778d17cb1a5c90851b
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-08-04 04:18:05 +0900

    cooooorsssss

--- app.ts
+++ app.ts
@@ -18,31 +18,13 @@ const passportSetup = require("./auth/passport");
 const app: Express = express();
 dotenv.config({ path: "./.env" });
 
-const corsConfig = {
-  origin: process.env.CORS_URL,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-  allowedHeaders: [
-    "Content-Type",
-    "Origin",
-    "X-Requested-With",
-    "Accept",
-    "x-client-key",
-    "x-client-token",
-    "x-client-secret",
-    "Authorization",
-  ],
-};
+app.use(
+  cors({
+    origin: process.env.CORS_URL,
+    credentials: true,
+  })
+);
 
-// app.use(
-//   cors({
-//     origin: process.env.CORS_URL,
-//     methods: "GET,POST,PATCH,DELETE",
-//     allowedHeaders: "Content-Type,Authorization",
-//     credentials: true,
-//   })
-// );
-app.use(cors(corsConfig));
 app.use(express.json());
 
 app.use(
@@ -57,7 +39,7 @@ app.use(passport.session());
 
 app.use("/auth", authRoutes);
 app.use("/dashboard", dashboardRoutes);
-app.options("*", cors(corsConfig));
+
 app.listen({ port: 5000, host: "0.0.0.0" }, () => {
   console.log("server is running");
 });
